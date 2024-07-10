005430277ceaa9ba02b4276c636606182e1c838d
Matvey Greyrat <greyrat999@mail.ru>
2024-07-10 14:27:10 +0300

    Заново добавляю все файлы

---
+++ first/git_test/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Отработка...")
+	fmt.Println("Изменение 2 в файле")
+	fmt.Println("Проработка reset")
+}
---
+++ first/git_test/second.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func second() {
+	fmt.Println("Отработка 2го файла...")
+}
